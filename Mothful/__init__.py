8edb9e64532d5edaed9ec6c6b701916338ba4763
Davi Castro <davialmeida@47jd@gmail.com>
2022-03-20 20:25:32 -0300

    feat: added configurations path to create database engine

--- Mothful/__init__.py
+++ Mothful/__init__.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 app = Flask(__name__)
 app.config.from_object('config')
+
+db = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+Session = sessionmaker(bind=db)
+session = Session()
 
 from . import views
