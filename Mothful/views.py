c0f32bffd8c0504054bb106ef3f816691be53bf1
Davi Castro <davialmeida@47jd@gmail.com>
2022-03-21 00:24:11 -0300

    feat: created routes(view function) to register users

--- Mothful/views.py
+++ Mothful/views.py
@@ -1,7 +1,12 @@
-from Mothful import app
+from Mothful import app, users
 from flask import jsonify
 
 
 @app.route('/', methods=['GET'])
 def root():
     return jsonify({'message': 'Running Project'})
+
+
+@app.route('/users', methods=['POST'])
+def login():
+    return users.post_user()
