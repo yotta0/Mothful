e1f2ec2b0b10a32c784e7bb0286b36f8c4bab68c
Davi Castro <davialmeida@47jd@gmail.com>
2022-03-21 00:25:41 -0300

    feat: updated users model class

--- Mothful/models.py
+++ Mothful/models.py
@@ -1,4 +1,4 @@
-from datetime import date, datetime
+from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, DateTime
 
@@ -13,3 +13,14 @@ class User(Base):
     name = Column(String(60), nullable=False)
     email = Column(String(50), unique=True, nullable=False)
     created_at = Column(DateTime(), default=datetime.now())
+    
+
+    def __init__(self, username, password, name, email):
+        self.username = username
+        self.password = password
+        self.name = name
+        self.email = email
+
+
+    def __repr__(self):
+        return f'username {self.username}'
