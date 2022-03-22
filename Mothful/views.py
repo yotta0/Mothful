45db15d3ffa3408b00b7fd6f3cad3ea7fd967666
Davi Castro <davialmeida@47jd@gmail.com>
2022-03-22 00:03:25 -0300

    feat: created views for  crud users in project API

--- Mothful/views.py
+++ Mothful/views.py
@@ -8,5 +8,17 @@ def root():
 
 
 @app.route('/users', methods=['POST'])
-def login():
+def post_user():
     return users.post_user()
+
+@app.route('/users/<id>', methods=['GET'])
+def get_user(id):
+    return users.get_user(id)
+
+@app.route('/users/<id>', methods=['PUT'])
+def update_user(id):
+    return users.update_user(id)
+
+@app.route('/users/<id>', methods=['DELETE'])
+def delete_user(id):
+    return users.delete_user(id)
